4c88cf1e49c0dc7965388e331c217a6dbb4b2dcf
josevictorbdbr <josevictorbdbr@gmail.com>
2026-07-06 12:09:53 -0300

    Fixes Domain name in some of the files

--- utils/siteConfig.ts
+++ utils/siteConfig.ts
@@ -1,4 +1,2 @@
-// Troque SITE_URL pelo domínio definitivo assim que ele for registrado.
-// O mesmo domínio também precisa ser atualizado em public/sitemap.xml e public/robots.txt.
-export const SITE_URL = "https://seudominio.com.br";
+export const SITE_URL = "https://imagechanger.com.br";
 export const SITE_NAME = "Image Changer";
